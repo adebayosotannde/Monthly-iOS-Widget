67712c9aceee9dd2b2ee97c82be63ebd85428fb1
Adebayo Sotannde <tsotannde@icloud.com>
2022-09-20 09:00:27 -0400

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Adebayo Sotannde on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
